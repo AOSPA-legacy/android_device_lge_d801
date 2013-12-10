41300d55e062c19c30e03cf8f196a8148cdea8e6
houstonn <houstonn@gmail.com>
2013-12-09 16:11:51 -0800

    g2: d801: PA bringup for building aospa
    *also squashed some CM updated commits
    
    Signed-off-by: houstonn <houstonn@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,9 +16,9 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := cyanogenmod_d801_defconfig
+TARGET_KERNEL_CONFIG := d801_defconfig
 
-TARGET_OTA_ASSERT_DEVICE := d801
+TARGET_OTA_ASSERT_DEVICE := g2,d801,galbi
 
 G2_DTS_TARGET := msm8974-g2-tmo_us
 
